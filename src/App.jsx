29a63102a86667674a29f61ba3aca9ecea35a0d1
Mustafa YILMAZ <myiistaken@gmail.com>
2022-09-07 23:56:09 +0300

    ui kit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Typography, CssBaseline, AppBar,Toolbar } from '@mui/material';
+
+const App = () => {
+    return (<>
+        <CssBaseline />
+        <AppBar position="relative">
+            <Toolbar>
+                <Typography variant='h2'>Hello World</Typography>
+            </Toolbar>
+
+        </AppBar>
+    </>);
+}
+
+export default App;
